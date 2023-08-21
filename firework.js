63c3a1c22497a959cc629f1b74435bd079e15893
Young <navskh@naver.com>
2023-08-22 01:23:24 +0900

    update

---
+++ firework.js
@@ -0,0 +1,59 @@
+document.addEventListener('click', function (event) {
+    createFirework(event.clientX, event.clientY);
+});
+
+function createFirework(x, y) {
+    const firework = document.createElement('div');
+    firework.classList.add('firework');
+    document.body.appendChild(firework);
+
+    firework.style.left = `${x}px`;
+    firework.style.bottom = `0px`;
+
+    setTimeout(() => {
+        explode(x, y);
+        firework.remove();
+    }, 1500);
+}
+
+function explode(x, y) {
+    for (let i = 0; i < 50; i++) {
+        createParticle(x, y);
+    }
+}
+
+function createParticle(x, y) {
+    const particle = document.createElement('div');
+    particle.classList.add('particle');
+    document.body.appendChild(particle);
+
+    const size = Math.random() * 5 + 5;
+    particle.style.width = `${size}px`;
+    particle.style.height = `${size}px`;
+    particle.style.background = randomRedTint();
+
+    const angle = random(0, 360);
+    const distance = random(50, 150);
+    const positionX =
+        x + distance * Math.cos((angle * Math.PI) / 180) - size / 2;
+    const positionY =
+        y + distance * Math.sin((angle * Math.PI) / 180) - size / 2;
+
+    particle.style.left = `${positionX}px`;
+    particle.style.top = `${positionY}px`;
+
+    setTimeout(() => {
+        particle.remove();
+    }, 3000);
+}
+
+function randomRedTint() {
+    const r = random(150, 255);
+    const g = random(0, 50);
+    const b = random(0, 50);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+function random(min, max) {
+    return Math.random() * (max - min) + min;
+}
--- stat.js
+++ stat.js
@@ -1,43 +1,59 @@
 // jQuery로 table의 행을 클릭하면 해당 행의 정보를 가져와서 modal에 뿌려준다.
 
 window.onload = async function () {
+    $('#todayDate').text(`(${new Date().toLocaleDateString()})`);
     // fetch 를 localhost:3000 을 호출하여 데이터를 가져온다.
     const response = await fetch('http://localhost:3000');
     const data = await response.json();
+    console.log(data);
+    for (let i = 0; i < 2; i++) {
+        const x = Math.random() * window.innerWidth;
+        const y = Math.random() * window.innerHeight;
+        createFirework(x, y);
+    }
 
     makeDCU(data);
 };
+
+function isDidIt(x) {
+    return x != '' && x != '#N/A';
+}
 
 // data 가지고 table 만들어주기
 function makeDCU(data) {
     data.forEach(element => {
         console.log(element);
-        const thisOYO = [];
-
         const thisBrightEmoji =
             brightEmoji[Math.floor(Math.random() * brightEmoji.length)];
         const thisSadEmoji =
             sadEmoji[Math.floor(Math.random() * sadEmoji.length)];
 
-        element[2] != ''
+        const thisBR = isDidIt(element[1]) ? element[1] : 0;
+
+        const thisOYO = [];
+        isDidIt(element[2])
             ? thisOYO.push(thisBrightEmoji)
             : thisOYO.push(thisSadEmoji);
-        element[3] != ''
+        isDidIt(element[3])
             ? thisOYO.push(thisBrightEmoji)
             : thisOYO.push(thisSadEmoji);
-        element[4] != ''
+        isDidIt(element[4])
             ? thisOYO.push(thisBrightEmoji)
             : thisOYO.push(thisSadEmoji);
 
         let prayer = '';
-        element[5] != '' ? (prayer = thisBrightEmoji) : (prayer = thisSadEmoji);
+        isDidIt(element[5]) != ''
+            ? (prayer = thisBrightEmoji)
+            : (prayer = thisSadEmoji);
+
+        let thisEV = isDidIt(element[6]) ? element[6].split('-') : [0, 0, 0, 0];
 
         $('#broList').append(`<tr>
           <td>${element[0]}</td>
-          <td>${element[1]}</td>
+          <td>${thisBR}</td>
           <td style="font-size: 25px">${thisOYO.join('-')}</td>
           <td style="font-size: 25px">${prayer}</td>
-          <td>${element[6]}</td>
+          <td>${thisEV.join('-')}</td>
         </tr>`);
     });
 }
